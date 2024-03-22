2bc51afec2904c181b34d714f887a324fd0fedd0
Macarena Capra Prince <162723190+macaprynce@users.noreply.github.com>
2024-03-22 12:00:55 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,14 @@
 def change():
     expense = 23.75
     money = 100
+    vuelto = money // expense
+    centavos=
+    print("ingresar gasto:")
+    print(expense)
+    print("dinero recibido")
+    print(money)
+    print("vuelto")
+    print("pesos:")
+    print(vuelto)
+    print("centavos:")
+    print(centavos)
