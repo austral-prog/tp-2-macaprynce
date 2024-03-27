0b972839dc520fcac523c40c45ba338f86f8e83b
Macarena Capra Prince <162723190+macaprynce@users.noreply.github.com>
2024-03-27 15:57:30 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,7 +1,7 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
-    print(f"{first_name.lower()} {last_name.lower()"} 
-    print(f"{first_name.capitalize()} {last_name.capitalize()"}
-    print(f"{first_name.upper()} {last_name.upper()"}
+    print(f"{first_name.lower()} {last_name.lower()"})
+    print(f"{first_name.capitalize()} {last_name.capitalize()"})
+    print(f"{first_name.upper()} {last_name.upper()"})
     
